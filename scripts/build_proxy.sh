2704eb12d61fd885d27adffe81fdcdd7f65845ef
Andrew Ferguson <13520045+andrewferguson@users.noreply.github.com>
2025-08-17 16:05:58 +0100

    Add build steps for 5G proxy and global slot checker

--- scripts/build_proxy.sh
+++ scripts/build_proxy.sh
@@ -24,20 +24,37 @@ function step_log() {
 GITHUB_TOKEN="$1"
 NUM_MACHINE="$2"
 GITHUB_USERNAME="$3"
-kernel_repo="andrewferguson/phobos-proxy"
-  sudo apt update 
-  sudo apt-get install -yqq libsctp-dev lksctp-tools  zlib1g-dev
-  sudo modprobe sctp
-phobos_link="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${kernel_repo}.git"
-git clone --quiet "${phobos_link}" ~/phobos-proxy
-cd ~/phobos-proxy
+sudo apt update
+sudo apt-get install -yqq libsctp-dev lksctp-tools  zlib1g-dev
+sudo modprobe sctp
 for (( i=0; i<NUM_MACHINE; i++ )); do
   DEST_NET=$((1 + i))
   GW_NET=$((1 + i))
   echo "Adding route: 10.2.${DEST_NET}.2 via 10.1.${GW_NET}.1"
   sudo ip route add 10.2."${DEST_NET}".2 via 10.1."${GW_NET}".1
 done
+
+# 4G
+repo="andrewferguson/phobos-proxy"
+git_url="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${repo}.git"
+git clone --quiet "${git_url}" ~/phobos-proxy
+cd ~/phobos-proxy
 make -j
+cd ~/
+
+# 5G
+repo="ujjwalpawar/phobos-5g"
+git_url="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${repo}.git"
+git clone --quiet "${phobos_link}" ~/phobos-5g
+cd ~/phobos-5g
+git checkout split
+make -j
+cd ~/
+
+# Global slot checker
+cd ~/phobos-5g
+gcc -o global_slotchecker src/sc_global.c
+
 sudo cp /local/repository/scripts/generate_conf.sh ~/
 cd ~/
 sudo chmod +x generate_conf.sh
